bd6b39570dc3a38a4961d4b327e5ea2644d388d3
lulili666 <79951049+lulili666@users.noreply.github.com>
2022-04-09 09:15:01 +0800

    main

---
+++ src/main/java/com/mypro/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.mypro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
