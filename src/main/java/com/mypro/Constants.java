b57cc15bed1b7ea518d765d50ceeb1891cbbd73c
lulili666 <79951049+lulili666@users.noreply.github.com>
2022-04-09 09:10:31 +0800

    commom

---
+++ src/main/java/com/mypro/Constants.java
@@ -0,0 +1,12 @@
+package com.example.mypro.common;
+
+/*
+   定义一些常量,与前端进行约定
+ */
+public interface Constants {
+    String CODE_200 = "200";
+    String CODE_500 = "500";
+    String CODE_400 = "400";   //参数错误
+    String CODE_401 = "401";  //权限不足
+    String CODE_600 = "600"; //其他问题
+}
---
+++ src/main/java/com/mypro/Result.java
@@ -0,0 +1,33 @@
+package com.example.mypro.common;
+
+/*
+   结果集统一向前端上传接口
+ */
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Result {
+    private String code;
+    private String msg;
+    private Object data;
+
+    public static Result success() {
+        return new Result(Constants.CODE_200,"",null);
+    }
+    public static Result success(Object data) {
+        return new Result(Constants.CODE_200,"",data);
+    }
+
+    public static Result error(String code,String msg) {
+        return new Result(code,msg,null);
+    }
+
+    public static Result error() {
+        return new Result(Constants.CODE_500, "系统错误", null);
+    }
+}
